13dc03445575888230110f33dd58f6e42998c6c5
vrom911 <veronika@holmusk.com>
2018-07-26 16:22:44 +0800

    Fix after review

--- src/Stan/Partial.hs
+++ src/Stan/Partial.hs
@@ -16,7 +16,7 @@ import HsInstances ()
 import Module (ModuleName, mkModuleName, moduleName)
 import Name (Name, nameModule, nameOccName, occNameString)
 import OccName (OccName, mkVarOcc)
-import SrcLoc (GenLocated (L), Located, RealSrcSpan, SrcSpan (..))
+import SrcLoc (GenLocated (L), Located, SrcSpan (..))
 
 import Stan.Warning (Warning (..))
 
@@ -28,11 +28,12 @@ findPartials = mapMaybe toWarning . lNames
     lNames = listify (\(_ :: Located Name) -> True)
 
     toWarning :: Located Name -> Maybe Warning
-    toWarning (L l name) =
+    toWarning (L l name) = do
         let modName = moduleName $ nameModule name
-            occName = nameOccName name
-        in guard (ifPartial modName occName && isRealSrcSpan l)
-               *> Just (Partial (occNameString occName) $ getReal l)
+        let occName = nameOccName name
+        RealSrcSpan src <- Just l
+        guard $ ifPartial modName occName
+        pure $ Partial (occNameString occName) src
          -- Just $ Partial (moduleNameString (moduleName $ nameModule name) ++ occNameString (nameOccName name))
 
 -- | The list of the partial functions and where they comes from.
@@ -53,11 +54,3 @@ ifPartial modName occName = any checkFun partialFuns
     checkFun :: (ModuleName, OccName) -> Bool
     checkFun (pModName, pOccName) = pModName == modName
                                  && pOccName == occName
-
-isRealSrcSpan :: SrcSpan -> Bool
-isRealSrcSpan (RealSrcSpan _) = True
-isRealSrcSpan _               = False
-
-getReal :: SrcSpan -> RealSrcSpan
-getReal (RealSrcSpan src) = src
-getReal _                 = error "Impossible happened"
