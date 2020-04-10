87ca4f05a2707fb054230ab1f6e1ea8642360003
Dmitrii Kovanikov <kovanikov@gmail.com>
2020-04-10 22:13:24 +0100

    [#5] Create basic command line interface
    Resolves #5

--- src/Stan.hs
+++ src/Stan.hs
@@ -1,7 +1,17 @@
+{- |
+Copyright: (c) 2020 Kowainik
+SPDX-License-Identifier: MPL-2.0
+Maintainer: Kowainik <xrom.xkov@gmail.com>
+
+Main running module.
+-}
+
 module Stan
        ( runStan
        ) where
 
+import Stan.Cli (runStanCli)
+
 
 runStan :: IO ()
-runStan = pass
+runStan = runStanCli >>= \_ -> pass
