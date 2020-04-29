2914d6121625b2c87c5cfe14bd8a700d0be18f2f
Veronika Romashkina <vrom911@gmail.com>
2020-04-29 10:02:35 +0100

    [#75] Find filepath to cabal file
    Resolves #75

--- src/Stan.hs
+++ src/Stan.hs
@@ -10,9 +10,11 @@ module Stan
     ( run
     ) where
 
-import Colourista (errorMessage, formatWith, italic)
+import Colourista (errorMessage, formatWith, infoMessage, italic, warningMessage)
 import Extensions (getPackageExtentionsBySources)
 import HieTypes (HieFile (..))
+import System.Directory (getCurrentDirectory, listDirectory)
+import System.FilePath (takeExtension, (</>))
 
 import Stan.Analysis (runAnalysis)
 import Stan.Analysis.Pretty (prettyShowAnalysis)
@@ -35,7 +37,15 @@ runStan StanArgs{..} = do
     -- create Map from filepathes to sources.
     let sourcesMap = fromList $ map (\HieFile{..} -> (hie_hs_file, hie_hs_src)) hieFiles
     -- create extensions map
-    extensionsMap <- getPackageExtentionsBySources "stan.cabal" sourcesMap
+    extensionsMap <- findCabalFile >>= \case
+        Just cabal -> do
+            infoMessage $ "Using the following .cabal file: " <> toText cabal <> "\n"
+            getPackageExtentionsBySources cabal sourcesMap
+        -- if cabal file is not found, pass the empty map instead
+        Nothing    -> do
+            warningMessage ".cabal file not found in the current directory."
+            infoMessage " 💡 Try using --cabal-file option to specify the path to the .cabal file.\n"
+            pure mempty
 
     let analysis = runAnalysis extensionsMap hieFiles
     putTextLn $ prettyShowAnalysis analysis stanArgsToggleSolution
@@ -48,4 +58,16 @@ runInspection InspectionArgs{..} = case inspectionArgsId of
         Just ins -> putTextLn $ prettyShowInspection ins
         Nothing  -> do
             errorMessage $ "Inspection with such ID does not exist: " <> unId insId
-            putTextLn $ formatWith [italic] "  Use 'stan inspection' to see the list of all available inspections."
+            putTextLn $ " 💡 " <> formatWith [italic] "Use 'stan inspection' to see the list of all available inspections."
+
+-- | Find a @.cabal@ file in the current directory.
+-- TODO: better error handling in stan.
+findCabalFile :: IO (Maybe FilePath)
+findCabalFile = do
+    dirPath <- getCurrentDirectory
+    dirContent <- listDirectory dirPath
+    let cabalFiles = filter (\p -> takeExtension p == ".cabal") dirContent
+    pure $ case cabalFiles of
+        []          -> Nothing -- throwError $ NoCabalFile dirPath
+        [cabalFile] -> Just $ dirPath </> cabalFile
+        x:_xs       -> Just x -- throwError $ MultipleCabalFiles (x :| xs)
