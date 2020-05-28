a657ad4c07ae5ff4e6e3a8dd82eea2441e9f960a
Dmitrii Kovanikov <kovanikov@gmail.com>
2020-05-28 20:59:22 +0100

    [#120] Create the data type to accumulate and aply config info
    Resolves #120

--- src/Stan/Config.hs
+++ src/Stan/Config.hs
@@ -32,6 +32,9 @@ module Stan.Config
 
       -- * Printing
     , configToCliCommand
+
+      -- * Apply config
+    , applyChecks
     ) where
 
 import Trial ((::-), Phase (..), Trial, withTag)
@@ -40,9 +43,12 @@ import Stan.Category (Category (..))
 import Stan.Core.Id (Id (..))
 import Stan.Core.ModuleName (ModuleName (..))
 import Stan.Inspection (Inspection (..))
+import Stan.Inspection.All (inspections, inspectionsIds, lookupInspectionById)
 import Stan.Observation (Observation (..))
 import Stan.Severity (Severity (..))
 
+import qualified Data.HashMap.Strict as HashMap
+import qualified Data.HashSet as HashSet
 import qualified Data.Text as T
 
 
@@ -128,3 +134,81 @@ configToCliCommand ConfigP{..} = "stan " <> T.intercalate " \\\n     " (map chec
         CheckScopeFile file -> " --file=" <> toText file
         CheckScopeDirectory dir -> " --directory=" <> toText dir
         CheckScopeModule m -> " --module=" <> unModuleName m
+
+{- | Convert the list of 'Check's from 'Config' to data structure that
+allows filtering of 'Inspection's.
+-}
+applyChecks :: [FilePath] -> [Check] -> HashMap FilePath (HashSet (Id Inspection))
+applyChecks paths = foldl' useCheck filesMap
+  where
+    filesMap :: HashMap FilePath (HashSet (Id Inspection))
+    filesMap = HashMap.fromList $ map (, inspectionsIds) paths
+
+    useCheck
+        :: HashMap FilePath (HashSet (Id Inspection))
+        -> Check
+        -> HashMap FilePath (HashSet (Id Inspection))
+    useCheck dict Check{..} =
+        applyForScope (applyFilter checkType checkFilter) checkScope dict
+
+    applyFilter
+        :: CheckType
+        -> Maybe CheckFilter
+        -> HashSet (Id Inspection)
+        -> HashSet (Id Inspection)
+    applyFilter = \case
+        Include -> includeFilter
+        Ignore -> ignoreFilter
+
+    ignoreFilter :: Maybe CheckFilter -> HashSet (Id Inspection) -> HashSet (Id Inspection)
+    ignoreFilter cFilter = HashSet.filter (not . satisfiesFilter cFilter)
+
+    includeFilter :: Maybe CheckFilter -> HashSet (Id Inspection) -> HashSet (Id Inspection)
+    includeFilter mFilter ins = case mFilter of
+         -- add all inspections to the existing ones
+         Nothing -> inspectionsIds <> ins
+         Just cFilter -> case cFilter of
+             CheckInspection iId -> HashSet.insert iId ins
+             CheckObservation _ -> ins
+             CheckSeverity sev ->
+                 let sevInspections = filter ((== sev) . inspectionSeverity) inspections
+                 in HashSet.fromList (map inspectionId sevInspections) <> ins
+             CheckCategory cat ->
+                 let catInspections = filter (elem cat . inspectionCategory) inspections
+                 in HashSet.fromList (map inspectionId catInspections) <> ins
+
+    -- Returns 'True' if the given inspection satisfies 'CheckFilter'
+    satisfiesFilter :: Maybe CheckFilter -> Id Inspection -> Bool
+    satisfiesFilter mFilter iId = case mFilter of
+        Nothing -> True  -- no filter, always satisfies
+        -- TODO: rewrite more efficiently after using GHC-8.10
+        Just cFilter -> case lookupInspectionById iId of
+            Nothing -> False  -- no such ID => doesn't satisfy
+            Just Inspection{..} -> case cFilter of
+                CheckInspection checkId -> iId == checkId
+                CheckObservation _      -> False
+                CheckSeverity sev       -> sev == inspectionSeverity
+                CheckCategory cat       -> elem cat inspectionCategory
+
+    applyForScope
+        :: (HashSet (Id Inspection) -> HashSet (Id Inspection))
+        -> Maybe CheckScope
+        -> HashMap FilePath (HashSet (Id Inspection))
+        -> HashMap FilePath (HashSet (Id Inspection))
+    applyForScope f mScope hm = case mScope of
+        Nothing -> f <$> hm  -- no scope = apply for everything
+        Just cScope -> case cScope of
+            CheckScopeFile path -> HashMap.alter (fmap f) path hm
+            CheckScopeDirectory dir ->
+                HashMap.fromList
+                $ mapKeepMaybe
+                    (\(path, ins) -> guard (isInDir dir path) *> pure (path, f ins))
+                $ HashMap.toList hm
+            CheckScopeModule _moduleName -> hm  -- TODO: no info about modules here :(
+
+    isInDir :: FilePath -> FilePath -> Bool
+    isInDir dir path = dir `isPrefixOf` path
+
+-- Like 'mapMaybe' but keeps the original element if function returns Nothing
+mapKeepMaybe :: (a -> Maybe a) -> [a] -> [a]
+mapKeepMaybe f = mapMaybe (\a -> f a <|> Just a)
