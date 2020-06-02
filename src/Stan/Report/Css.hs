c439543e398d4233ab088dd8aab1fb41ee2f57a8
Veronika Romashkina <vrom911@gmail.com>
2020-06-02 21:45:23 +0100

    Fix

--- src/Stan/Report/Css.hs
+++ src/Stan/Report/Css.hs
@@ -1,28 +1,36 @@
+{- |
+Copyright: (c) 2020 Kowainik
+SPDX-License-Identifier: MPL-2.0
+Maintainer: Kowainik <xrom.xkov@gmail.com>
+
+Css to be used in the generated HTML in the report.
+-}
 module Stan.Report.Css
     ( stanCss
     ) where
 
-import Clay (Css, (?))
-
-import qualified Clay as Css
+import Clay (Color, Css, Percentage, Size, backgroundColor, block, body, borderTop, center, color,
+             display, fontFamily, footer, hsl, margin, px, sansSerif, solid, textAlign, white,
+             width, (?))
 
 
 stanCss :: Css
 stanCss = do
-    Css.body ? do
-        Css.margin zero zero zero zero
-        Css.fontFamily [] [Css.sansSerif]
-        Css.color darkGrey
-    Css.footer ? do
-        Css.display Css.block
-        Css.textAlign Css.center
-        Css.width (100 :: Css.Size Css.Percentage)
-        Css.color Css.white
-        Css.backgroundColor lightGrey
-        Css.borderTop Css.solid (Css.px 8) darkGrey
+    body ? do
+        margin zero zero zero zero
+        fontFamily [] [sansSerif]
+        color darkGrey
+    footer ? do
+        display block
+        textAlign center
+        width (100 :: Size Percentage)
+        color white
+        backgroundColor lightGrey
+        borderTop solid (px 8) darkGrey
   where
-    zero :: Css.Size Css.Percentage
+    zero :: Size Percentage
     zero = 0
 
-    lightGrey = Css.hsl 0 0 74
-    darkGrey = Css.hsl 0 0 38
+    lightGrey, darkGrey :: Color
+    lightGrey = hsl 0 0 74
+    darkGrey = hsl 0 0 38
