6d9686d97c02aeb4556a26136fa4db1984d13d0c
Veronika Romashkina <vrom911@gmail.com>
2020-06-04 15:25:45 +0100

    [#189] Create Used Inspections Section (#203)
    Resolves #189

--- src/Stan/Report/Html.hs
+++ src/Stan/Report/Html.hs
@@ -16,8 +16,9 @@ module Stan.Report.Html
 
 import Clay (render)
 import Data.Char (toLower)
-import Html (a_A, body_, div_A, doctype_, footer_, h1_, h2_, h3_, head_, header_, html_, li_, main_,
-             meta_A, p_, pre_, strong_, style_, table_, td_, title_, tr_, ul_, ( # ))
+import Html (a_A, body_, div_, div_A, doctype_, em_, footer_, h1_, h2_, h3_, head_, header_, hr_,
+             html_, li_, main_, meta_A, p_, pre_, span_A, strong_, style_, table_, td_, title_, tr_,
+             ul_, ( # ))
 
 import Stan.Analysis (Analysis (..))
 import Stan.Category (Category (..))
@@ -31,7 +32,6 @@ import Stan.Report.Css (stanCss)
 
 import qualified Data.List as List (words)
 import qualified Data.Map.Strict as Map
-import qualified Data.Text as T
 import qualified Html.Attribute as A
 
 
@@ -39,19 +39,19 @@ stanHtml an = doctype_ # html_ (stanHead # stanBody)
   where
     stanBody = body_ (stanHeader # stanMain an # stanFooter)
 
-stanHeader = header_ (h1_ "Stan Report")
+stanHeader = header_ (h1_ "Stan Report" # hr_)
 
 stanMain an = main_
     ( divIdClass "general-info" ""
-      ( divIdClassH "Project Info" "" ()
-      # divIdClassH "Stan Info" "" ()
-      # divIdClassH "Analysis Info" "" ()
-      )
+        ( divIdClassH "Project Info" "" ()
+        # divIdClassH "Stan Info" "" ()
+        # divIdClassH "Analysis Info" "" ()
+        )
     # divIdClassH "Graphs" "" (p_ "Maybe later")
     # divIdClassH "Observations" "" (stanObservations an)
     # divIdClassH "Configurations" "" (p_ "Later")
     # divIdClassH "Summary" "" (p_ "Later")
-    # divIdClassH "Inspections" "" (p_ "Later")
+    # divIdClassH "Inspections" "" (stanInspections $ analysisInspections an)
     )
 
 stanObservations Analysis{..} =
@@ -70,20 +70,15 @@ stanPerFile FileInfo{..} = divIdClass "" "" ( h3_ fileInfoPath # ul_
 stanObservation o@Observation{..} = divIdClass (unId observationId) "observation"
     ( general
     # pre_ (unlines $ prettyObservationSource False o)
-    # nothingIfTrue (null solutions) (divClass "solutions"
-        ( p_ "Possible solution"
-        # ul_ (map li_ solutions)
-        ))
+    # solutionsDiv inspection
     )
   where
     general = divClass "observation-general" $ table_
         ( tr_ (td_ "ID " # td_ (strong_ $ unId observationId))
-        # tr_ (td_ "Severity" # td_ (show @Text $ inspectionSeverity inspection))
+        # tr_ (td_ "Severity" # td_ (severity inspection))
         # tr_ (td_ "Description" # td_ (inspectionDescription inspection))
         # tr_ (td_ "Inspection ID" # td_ (a_A (A.href_ $ toText "#" <> insId) insId))
-        # tr_ (td_ "Category" # td_ (toText "#" <> T.intercalate (toText " #")
-                  (map unCategory $ toList $ inspectionCategory inspection))
-              )
+        # tr_ (td_ "Category" # td_ (categories inspection))
         # tr_ (td_ "File" # td_ observationFile)
         )
 
@@ -93,16 +88,33 @@ stanObservation o@Observation{..} = divIdClass (unId observationId) "observation
     insId :: Text
     insId = unId $ observationInspectionId
 
-    solutions :: [Text]
-    solutions = inspectionSolution inspection
-
-divClass c = div_A (A.class_ c)
-divIdClass i c = div_A (A.id_ i # A.class_ c)
-
-divIdClassH h c rest = divIdClass i c (h2_ h # rest)
+severity ins = span_A (A.class_ $ toText "severity" <> severityTxt) severityTxt
   where
-    i :: String
-    i = intercalate "-" $ map (map toLower) $ List.words h
+    severityTxt :: Text
+    severityTxt = show $ inspectionSeverity ins
+
+categories ins = map (span_A (A.class_ "cat") . unCategory) $
+    toList $ inspectionCategory ins
+
+solutionsDiv ins = nothingIfTrue (null solutions)
+    ( divClass "solutions"
+        ( p_ "Possible solution"
+        # ul_ (map li_ solutions)
+        )
+    )
+  where
+    solutions :: [Text]
+    solutions = inspectionSolution ins
+
+stanInspections = div_ . map stanInspection . sortWith unId . toList
+stanInspection (getInspectionById -> ins@Inspection{..}) = divIdClass (unId inspectionId) "inspection"
+    ( h3_ ("Inspection " # unId inspectionId)
+    # p_ (strong_ inspectionName)
+    # p_ (em_ inspectionDescription)
+    # p_ (severity ins)
+    # p_ (categories ins)
+    # solutionsDiv ins
+    )
 
 stanFooter = footer_ ("(c) Kowainik 2020")
 
@@ -119,6 +131,15 @@ stanHead = head_
     )
   where
     nameContent x y = meta_A (A.name_ x # A.content_ y)
+
+divClass c = div_A (A.class_ c)
+divIdClass i c = div_A (A.id_ i # A.class_ c)
+
+divIdClassH h c rest = divIdClass i c (h2_ h # rest)
+  where
+    i :: String
+    i = intercalate "-" $ map (map toLower) $ List.words h
+
 
 nothingIfTrue :: Bool -> a -> Maybe a
 nothingIfTrue p a = if p then Nothing else Just a
