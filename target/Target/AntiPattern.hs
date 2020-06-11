9a136acc39bb67dc38113d1b2eff734cae2753f9
Veronika Romashkina <vrom911@gmail.com>
2020-06-11 15:49:12 +0100

    [#99] Add nub inspection (#249)

--- target/Target/AntiPattern.hs
+++ target/Target/AntiPattern.hs
@@ -5,6 +5,7 @@ module Target.AntiPattern where
 import qualified Data.ByteString.Char8 as BS8
 import qualified Data.HashMap.Strict as HM
 import qualified Data.HashSet as HS
+import qualified Data.List as List
 import qualified Data.Text as Text
 
 
@@ -43,3 +44,6 @@ stanEitherFoldr = foldr (+) 0
 
 stanTextLength :: Text.Text -> Int
 stanTextLength = Text.length
+
+stanNub :: [Int] -> [Int]
+stanNub = List.nub
