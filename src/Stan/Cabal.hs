a08e5b988c7d8ac15d7fc7138a75305835b501d8
0rphee <0rph3e@proton.me>
2025-05-21 04:12:10 -0600

    Improve finding cabal files (#586)
    Co-authored-by: @Bodigrim

--- src/Stan/Cabal.hs
+++ src/Stan/Cabal.hs
@@ -6,6 +6,11 @@ Maintainer: Kowainik <xrom.xkov@gmail.com>
 Functions to work with cabal files and cabal extension maps.
 -}
 
+{-# LANGUAGE CPP #-}
+#ifdef MIN_VERSION_directory_ospath_streaming
+{-# LANGUAGE QuasiQuotes #-}
+#endif
+
 module Stan.Cabal
     ( createCabalExtensionsMap
     , usedCabalFiles
@@ -19,14 +24,22 @@ import Colourista (errorMessage, infoMessage, warningMessage)
 import Control.Exception (catch)
 import Extensions (CabalException, ExtensionsError (..), ExtensionsResult, ParsedExtensions (..),
                    mergeAnyExtensions, parseCabalFileExtensions)
-import System.Directory (doesDirectoryExist, doesFileExist, getCurrentDirectory, listDirectory,
-                         makeRelativeToCurrentDirectory)
-import System.FilePath (takeExtension, (</>))
-import System.IO.Unsafe (unsafeInterleaveIO)
+import System.Directory (doesFileExist, makeRelativeToCurrentDirectory)
 
 import Stan.Hie.Compat (HieFile (..))
 
 import qualified Data.Map.Strict as Map
+#ifndef MIN_VERSION_directory_ospath_streaming
+import System.IO.Unsafe (unsafeInterleaveIO)
+import qualified System.Directory as FilePath
+import qualified System.FilePath as FilePath
+#else
+import qualified System.OsPath as OsPath
+import qualified System.Directory.OsPath as OsPath
+import qualified System.Directory.OsPath.Streaming as OPS
+import qualified System.Directory.OsPath.Types as OPS
+import qualified Data.Set as S
+#endif
 
 
 {- | Gets the list of @.cabal@ file paths that were used in the project.
@@ -84,46 +97,92 @@ createCabalExtensionsMap isLoud cabalPath hies = case cabalPath of
 subdirectories. It returns maximum 1 @.cabal@ file from each directory.
 -}
 findCabalFiles :: IO [FilePath]
-findCabalFiles = do
-    dir <- getCurrentDirectory
-    curDirCabal <- findCabalFileDir dir
-    dirs <- getSubdirsRecursive dir
-    subDirsCabals <- mapM findCabalFileDir dirs
+findCabalFiles =
+#ifndef MIN_VERSION_directory_ospath_streaming
+    findCabalFilesFilePath
+#else
+    findCabalFilesStreaming
+#endif
+
+#ifndef MIN_VERSION_directory_ospath_streaming
+findCabalFilesFilePath :: IO [FilePath]
+findCabalFilesFilePath = do
+    dir <- FilePath.getCurrentDirectory
+    curDirCabal <- findCabalFileFilePath dir
+    dirs <- getSubdirsRecursiveFilePath dir
+    subDirsCabals <- mapM findCabalFileFilePath dirs
     pure $ catMaybes $ curDirCabal : subDirsCabals
-
--- | Find a @.cabal@ file in the given directory.
--- TODO: better error handling in stan.
-findCabalFileDir :: FilePath -> IO (Maybe FilePath)
-findCabalFileDir dir = do
-    dirContent <- listDirectory dir
-    let cabalFiles = filter isCabal dirContent
-    pure $ case cabalFiles of
-        []          -> Nothing -- throwError $ NoCabalFile dirPath
-        [cabalFile] -> Just $ dir </> cabalFile
-        x:_xs       -> Just x -- throwError $ MultipleCabalFiles (x :| xs)
   where
-    isCabal :: FilePath -> Bool
-    isCabal p = takeExtension p == ".cabal"
+    -- | Find a @.cabal@ file in the given directory.
+    -- TODO: better error handling in stan.
+    findCabalFileFilePath :: FilePath -> IO (Maybe FilePath)
+    findCabalFileFilePath dir = do
+        dirContent <- FilePath.listDirectory dir
+        let cabalFiles = filter isCabal dirContent
+        pure $ case cabalFiles of
+            []            -> Nothing
+            cabalFile : _ -> Just $ dir FilePath.</> cabalFile
+      where
+        isCabal :: FilePath -> Bool
+        isCabal p = FilePath.takeExtension p == ".cabal"
 
-getSubdirsRecursive :: FilePath -> IO [FilePath]
-getSubdirsRecursive fp = do
-    all' <- filter nonGenDir <$> listDirectory fp
-    dirs <- filterM doesDirectoryExist (mkRel <$> all')
-    case dirs of
-        [] -> pure []
-        ds -> do
-            -- unsafeInterleaveIO is required here for performance reasons
-            next <- unsafeInterleaveIO $ foldMapA getSubdirsRecursive ds
-            pure $ dirs ++ next
+    getSubdirsRecursiveFilePath :: FilePath -> IO [FilePath]
+    getSubdirsRecursiveFilePath fp = do
+        all' <- filter nonGenDir <$> FilePath.listDirectory fp
+        dirs <- filterM FilePath.doesDirectoryExist (mkRel <$> all')
+        case dirs of
+            [] -> pure []
+            ds -> do
+                -- unsafeInterleaveIO is required here for performance reasons
+                next <- unsafeInterleaveIO $ foldMapA getSubdirsRecursiveFilePath ds
+                pure $ dirs ++ next
+      where
+        nonGenDir :: FilePath -> Bool
+        nonGenDir d =
+               d /= "dist"
+            && d /= "dist-newstyle"
+            && d /= ".stack-work"
+            && d /= ".git"
+
+        mkRel :: FilePath -> FilePath
+        mkRel = (fp FilePath.</>)
+
+#else
+-- Fix for https://github.com/haskell/haskell-language-server/issues/4515
+findCabalFilesStreaming :: IO [FilePath]
+findCabalFilesStreaming = do
+    setRef <- newIORef S.empty -- stores the directories where we already found 1 cabal file
+    root <- OsPath.getCurrentDirectory
+    traverse OsPath.decodeFS =<<
+        OPS.listContentsRecFold
+           Nothing -- Depth limit
+            (\_ _ (OPS.Relative _dirRelPath) (OPS.Basename dirBasename)  _symlinkType _consDirToList traverseThisSubdir rest ->
+                 if visitCurrSubdirPred dirBasename -- if this condition is satisfied
+                 then traverseThisSubdir rest -- True -> then this subdir will be traversed
+                 else rest -- False -> else, this subdir will not be traversed
+                ) -- how to fold this directory and its children, given its path
+            (\_ _ (OPS.Relative path) (OPS.Basename fileBasename) _ft -> do
+                  let parentDir = OsPath.takeDirectory path
+                  set <- readIORef setRef
+                  if not (S.member parentDir set) && collectPred fileBasename -- if this condition is satisfied
+                  then do
+                        writeIORef setRef $  S.insert parentDir set -- we add the parentDir of this file, to prevent adding more than 1 .cabal file
+                        pure (Just path) -- True -> then this path will be added to the results (because @path@ is already relative, we no longer need @mkRel@)
+                  else pure Nothing -- False -> else, this path wont be added
+                )
+            (Identity root) -- (f a), list of roots to search in
   where
-    nonGenDir :: FilePath -> Bool
-    nonGenDir d =
-           d /= "dist"
-        && d /= "dist-newstyle"
-        && d /= ".stack-work"
+    visitCurrSubdirPred :: OsPath.OsPath -> Bool
+    visitCurrSubdirPred d =
+           d /= [OsPath.osp|dist|]
+        && d /= [OsPath.osp|dist-newstyle|]
+        && d /= [OsPath.osp|.stack-work|]
+        && d /= [OsPath.osp|.git|]
 
-    mkRel :: FilePath -> FilePath
-    mkRel = (fp </>)
+    collectPred :: OsPath.OsPath -> Bool
+    collectPred p =
+        OsPath.takeExtension p == [OsPath.osp|.cabal|]
+#endif
 
 mergeParsedExtensions
     :: Either ExtensionsError ParsedExtensions
